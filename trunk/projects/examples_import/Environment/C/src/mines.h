2d7407fbc9eaaef8c1e226e9883f76dc220c30d6
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-09-06 01:45:18 +0000

    cleaned up the sample environment to not have all of the env funcs redefined and also so that it would build with C or C++

--- trunk/projects/examples_import/Environment/C/src/mines.h
+++ trunk/projects/examples_import/Environment/C/src/mines.h
@@ -18,18 +18,16 @@
 #ifndef mines_h
 #define mines_h
 
-#include <RL_common.h>
+#ifdef __cplusplus
+  extern "C" {
+#endif
 
-/* RL-Glue interface */
-Task_specification env_init();
-Observation env_start();
-Reward_observation env_step(Action a);
-void env_cleanup();
-void env_set_state(State_key sk);
-void env_set_random_seed(Random_seed_key rsk);
-State_key env_get_State();
-Random_seed_key env_get_random_seek();
-Message env_message(const Message inMessage);
+#include <rlglue/Environment_common.h>
+
+#ifdef __cplusplus
+}
+#endif
+
 
 /* Mines utility functions */
 void env_print(const char* header, RL_abstract_type* data);
