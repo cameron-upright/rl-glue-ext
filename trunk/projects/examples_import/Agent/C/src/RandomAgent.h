cb83a2d33c4ebaae80bcc74c63a2aac353f5b735
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-09-06 01:46:19 +0000

    took out the agent funcs that are defined in Agent_common

--- trunk/projects/examples_import/Agent/C/src/RandomAgent.h
+++ trunk/projects/examples_import/Agent/C/src/RandomAgent.h
@@ -28,12 +28,6 @@ extern "C" {
 }
 #endif
 
-void agent_init(const Task_specification task_spec);
-Action agent_start(Observation o);
-Action agent_step(Reward r, Observation o);
-void agent_end(Reward r);
-void agent_cleanup();
-Message agent_message(const Message msg);
 int egreedy(Observation o);
 
 #endif
