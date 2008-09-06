a20fec88e9cd7a71c0b2c24e3ac672903fbbf440
brian <brian@tannerpages.com>
2008-09-06 20:21:07 +0000

    part way through cleaning up the top level directory of the C codec by using a config subdirectory

--- projects/codecs/C/code/rlglue/utils/C/RLStruct_util.h
+++ projects/codecs/C/code/rlglue/utils/C/RLStruct_util.h
@@ -1,8 +1,15 @@
 #ifndef RLStruct_Util_H
 #define RLStruct_Util_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <rlglue/RL_common.h>
 
 RL_abstract_type copyRLStruct(RL_abstract_type newStruct);
 
+#ifdef __cplusplus
+}
+#endif
 #endif
