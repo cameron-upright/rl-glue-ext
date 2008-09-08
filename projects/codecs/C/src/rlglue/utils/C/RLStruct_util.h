92278dd4fdd88a58d6b357b0ab2e4529e4df2578
brian <brian@tannerpages.com>
2008-09-08 22:29:29 +0000

    Updated RL_network.c to handle strings in rl_abstract_type_t.  Also made comments in RLStruct_util that I think they are way broken.

--- projects/codecs/C/src/rlglue/utils/C/RLStruct_util.h
+++ projects/codecs/C/src/rlglue/utils/C/RLStruct_util.h
@@ -6,7 +6,7 @@ extern "C" {
 #endif
 
 #include <rlglue/RL_common.h>
-
+/* As of Sept 8/2008 I think this function might be very broken */
 rl_abstract_type_t copyRLStruct(rl_abstract_type_t newStruct);
 
 #ifdef __cplusplus
