fcdc6cd1d4011ac268ca3f699dd2c95d240bc7ee
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-09-17 18:02:29 +0000

    Adding in a python script for doing massive cross-testing of codecs, and also partial implementation of the java test suite

--- trunk/projects/crossTesting/test_1_java_agent_c_environment_c_experiment.bash
+++
@@ -1,9 +0,0 @@
-killall rl_glue
-sleep 1
-rl_glue &
-sleep 1
-../codecs/C/tests/test_1_environment &
-sleep 1
-java -Xmx128M -classpath ../codecs/Java/dist/JavaCodec.jar org.rlcommunity.rlglue.tests.Test_1_Agent &
-sleep 1
-../codecs/C/tests/.libs/test_1_experiment
--- trunk/projects/crossTesting/test_message_java_agent_c_environment.bash
+++
@@ -1,9 +0,0 @@
-killall rl_glue
-sleep 1
-rl_glue &
-sleep 1
-../codecs/C/tests/test_message_environment &
-sleep 1
-java -Xmx128M -classpath ../codecs/Java/dist/JavaCodec.jar org.rlcommunity.rlglue.RLGlueCore --agent org.rlcommunity.rlglue.tests.Test_Message_Agent &
-sleep 1
-../codecs/C/tests/.libs/test_message_experiment
