91e6f0dd141b3125fb2930b5973b0dc9f7260a64
brian <brian@tannerpages.com>
2008-09-24 01:30:13 +0000

    Moved reloadPath out of the src directory so that the src is all codec and test stuff.

--- projects/codecs/Matlab/reloadPath.m
+++ projects/codecs/Matlab/reloadPath.m
@@ -1,4 +1,6 @@
 function reloadPath()
     clear java;javaaddpath('/Users/btanner/Documents/Java-Projects/rl-glue-ext/projects/codecs/Java/products/JavaRLGlueCodec.jar')
-    addpath(sprintf('%s%s',pwd(),'/glue'));
+    addpath(sprintf('%s%s',pwd(),'/src/glue'));
+    addpath(sprintf('%s%s',pwd(),'/src/agent'));
+    addpath(sprintf('%s%s',pwd(),'/src/environment'));
 end
