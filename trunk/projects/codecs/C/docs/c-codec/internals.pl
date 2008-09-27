3af32e41666803d9628ae33c1d3a059ab9954a76
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-09-27 23:29:50 +0000

    Adding the C codec docs as a test of my new config file

---
+++ trunk/projects/codecs/C/docs/c-codec/internals.pl
@@ -0,0 +1,26 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/exp/;
+$ref_files{$key} = "$dir".q|node37.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/env/;
+$ref_files{$key} = "$dir".q|node9.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ref/;
+$ref_files{$key} = "$dir".q|node49.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/agent/;
+$ref_files{$key} = "$dir".q|node24.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/quick/;
+$ref_files{$key} = "$dir".q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
