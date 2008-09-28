c5aada74ab5c89309da63e41b47aa926ebef1efc
brian <brian@tannerpages.com>
2008-09-28 09:19:50 +0000

    Adding the current version of the docs to svn.

---
+++ projects/codecs/C/docs/html/images.pl
@@ -0,0 +1,24 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/rightarrow;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\rightarrow$">|; 
+
+$key = q/geq;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$\geq$">|; 
+
+$key = q/neq;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="18" HEIGHT="34" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\neq$">|; 
+
+1;
+
