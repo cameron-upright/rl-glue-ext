e3e201b3b906b2c4f7e4dd4e1a298ca187ea1ce2
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-09-29 00:20:35 +0000

    Updated all the codec tests to use the same rl_episode test as the C codec (because we updated it and changed the step counting behavior).  Also made the cross language test python script allow for specifying only certain tests to run... like: ./testCodecs.py rl_episode message

--- trunk/projects/codecs/Python/tests/test_rl_episode_experiment.py
+++ trunk/projects/codecs/Python/tests/test_rl_episode_experiment.py
@@ -27,7 +27,7 @@ task_spec=RLGlue.RL_init()
 
 isTerminal = RLGlue.RL_episode(0);
 tester.check_fail(isTerminal!=1);
-tester.check_fail(RLGlue.RL_num_steps()!=6);
+tester.check_fail(RLGlue.RL_num_steps()!=5);
 
 isTerminal = RLGlue.RL_episode(1);
 tester.check_fail(isTerminal!=0);
@@ -37,17 +37,21 @@ isTerminal = RLGlue.RL_episode(2);
 tester.check_fail(isTerminal!=0);
 tester.check_fail(RLGlue.RL_num_steps()!=2);
 
+isTerminal = RLGlue.RL_episode(4);
+tester.check_fail(isTerminal!=0);
+tester.check_fail(RLGlue.RL_num_steps()!=4);
+
 isTerminal = RLGlue.RL_episode(5);
 tester.check_fail(isTerminal!=0);
 tester.check_fail(RLGlue.RL_num_steps()!=5);
 
 isTerminal = RLGlue.RL_episode(6);
 tester.check_fail(isTerminal!=1);
-tester.check_fail(RLGlue.RL_num_steps()!=6);
+tester.check_fail(RLGlue.RL_num_steps()!=5);
 
 isTerminal = RLGlue.RL_episode(7);
 tester.check_fail(isTerminal!=1);
-tester.check_fail(RLGlue.RL_num_steps()!=6);
+tester.check_fail(RLGlue.RL_num_steps()!=5);
 
 
 print tester.get_summary()
