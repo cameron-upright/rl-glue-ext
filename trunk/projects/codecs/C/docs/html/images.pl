bfc16f5b00359a5a140e10dffb26a3d314bce13a
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-09-29 21:43:57 +0000

    Updated math image to not have underline as per: http://ccrma.stanford.edu/~jos/webpub/Eliminating_Black_Rules_Under.html

--- trunk/projects/codecs/C/docs/html/images.pl
+++ trunk/projects/codecs/C/docs/html/images.pl
@@ -4,7 +4,7 @@
 
 $key = q/{0,1,ldots,19,20};MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="128" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="128" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="$\{0, 1,\ldots,19,20\}$">|; 
 
