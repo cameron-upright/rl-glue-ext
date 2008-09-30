06b09110c39f419779e329dc506bc2e0a46fe408
brian <brian@tannerpages.com>
2008-09-30 19:45:08 +0000

    First commit of Python docs (they just copies of Java docs so far)

---
+++ projects/codecs/Python/docs/html/internals.pl
@@ -0,0 +1,14 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate internals original text with physical files.
+
+
+$key = q/sec:agent/;
+$ref_files{$key} = "$dir".q|PythonCodec.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:structure-types/;
+$ref_files{$key} = "$dir".q|PythonCodec.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ projects/codecs/Python/docs/html/labels.pl
@@ -0,0 +1,21 @@
+# LaTeX2HTML 2002-2-1 (1.71)
+# Associate labels original text with physical files.
+
+
+$key = q/sec:agent/;
+$external_labels{$key} = "$URL/" . q|PythonCodec.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:structure-types/;
+$external_labels{$key} = "$URL/" . q|PythonCodec.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.71)
+# labels from external_latex_labels array.
+
+
+1;
+
