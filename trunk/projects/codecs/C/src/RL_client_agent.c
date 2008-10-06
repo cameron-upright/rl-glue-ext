bae0a1261092c6e7491f5941804cbb9e2020c71f
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2008-10-06 04:16:39 +0000

    Did a bunch of formatting to make things pretty, and replaced laborious freeing of data structures with calls to the utils library.

--- trunk/projects/codecs/C/src/RL_client_agent.c
+++ trunk/projects/codecs/C/src/RL_client_agent.c
@@ -35,163 +35,163 @@
 
 #include <rlglue/Agent_common.h>
 #include <rlglue/network/RL_network.h>
+/* Include the utility methods*/
+#include <rlglue/utils/C/RLStruct_util.h>
 
 
+/*
+This is the core of the agent C codec. It is very similar to RL_client_environment.c, so
+it may be slightly more sparsely commented than that file */
 
 static const char* kUnknownMessage = "Unknown Message: %d\n";
 
 static char* theTaskSpec = 0;
-static observation_t theObservation = {0};
-static rlBuffer theBuffer = {0};
-static message_t theInMessage = 0;
-static unsigned int theInMessageCapacity = 0;
+static observation_t clientagent_observation = {0};
+static rlBuffer clientagent_rlbuffer = {0};
+static message_t clientagent_inmessage = 0;
+static unsigned int clientagent_inmessagecapacity = 0;
 
 static void onAgentInit(int theConnection) {
-  unsigned int theTaskSpecLength = 0;
-  unsigned int offset = 0;
+	unsigned int theTaskSpecLength = 0;
+	unsigned int offset = 0;
 
-  /* Read the data in the buffer (data from server) */
-  offset = rlBufferRead(&theBuffer, offset, &theTaskSpecLength, 1, sizeof(int));
-  if (theTaskSpecLength > 0) {
-    theTaskSpec = (char*)calloc(theTaskSpecLength+1, sizeof(char));
-    offset = rlBufferRead(&theBuffer, offset, theTaskSpec, theTaskSpecLength, sizeof(char));
-	/*Added by Brian Tanner Sept 12 2008 to match some other places where we do the same thing */
-	theTaskSpec[theTaskSpecLength]='\0';
-  }
+	/* Read the data in the buffer (data from server) */
+	offset = rlBufferRead(&clientagent_rlbuffer, offset, &theTaskSpecLength, 1, sizeof(int));
+	if (theTaskSpecLength > 0) {
+		theTaskSpec = (char*)calloc(theTaskSpecLength+1, sizeof(char));
+		offset = rlBufferRead(&clientagent_rlbuffer, offset, theTaskSpec, theTaskSpecLength, sizeof(char));
+		/*Make sure the string is null terminated */
+		theTaskSpec[theTaskSpecLength]='\0';
+	}
 
-  /* Call RL method on the recv'd data */
-  agent_init(theTaskSpec);
+	/* Call RL method on the recv'd data */
+	agent_init(theTaskSpec);
 
-  /* Prepare the buffer for sending data back to the server */
-  rlBufferClear(&theBuffer);
+	/* Prepare the buffer for sending data back to the server */
+	rlBufferClear(&clientagent_rlbuffer);
 }
 
 static void onAgentStart(int theConnection) {
-  action_t theAction = {0};
-  unsigned int offset = 0;
+	action_t theAction = {0};
+	unsigned int offset = 0;
 
-  /* Read the data in the buffer (data from server) */
-  offset = rlCopyBufferToADT(&theBuffer, offset, &theObservation);
-	__RL_CHECK_STRUCT(&theObservation)
+	/* Read the data in the buffer (data from server) */
+	offset = rlCopyBufferToADT(&clientagent_rlbuffer, offset, &clientagent_observation);
+	__RL_CHECK_STRUCT(&clientagent_observation)
 
-  /* Call RL method on the recv'd data */
-  theAction = agent_start(theObservation);
+	/* Call RL method on the recv'd data */
+	theAction = agent_start(clientagent_observation);
 	__RL_CHECK_STRUCT(&theAction)
 
-  /* Prepare the buffer for sending data back to the server */
-  rlBufferClear(&theBuffer);
-  offset = 0;
-  offset = rlCopyADTToBuffer(&theAction, &theBuffer, offset);
+	/* Prepare the buffer for sending data back to the server */
+	rlBufferClear(&clientagent_rlbuffer);
+	offset = 0;
+	offset = rlCopyADTToBuffer(&theAction, &clientagent_rlbuffer, offset);
 }
 
 static void onAgentStep(int theConnection) {
-  reward_t theReward = 0;
-  action_t theAction = {0};
-  unsigned int offset = 0;
+	reward_t theReward = 0;
+	action_t theAction = {0};
+	unsigned int offset = 0;
 
-  /* Read the data in the buffer (data from server) */
-  offset = rlBufferRead(&theBuffer, offset, &theReward, 1, sizeof(theReward));
-  offset = rlCopyBufferToADT(&theBuffer, offset, &theObservation);
-	__RL_CHECK_STRUCT(&theObservation)
+	/* Read the data in the buffer (data from server) */
+	offset = rlBufferRead(&clientagent_rlbuffer, offset, &theReward, 1, sizeof(theReward));
+	offset = rlCopyBufferToADT(&clientagent_rlbuffer, offset, &clientagent_observation);
+	__RL_CHECK_STRUCT(&clientagent_observation)
 
-  /* Call RL method on the recv'd data */
-  theAction = agent_step(theReward, theObservation);
+	/* Call RL method on the recv'd data */
+	theAction = agent_step(theReward, clientagent_observation);
 	__RL_CHECK_STRUCT(&theAction)
 
-  /* Prepare the buffer for sending data back to the server */
-  rlBufferClear(&theBuffer);
-  offset = 0;
+	/* Prepare the buffer for sending data back to the server */
+	rlBufferClear(&clientagent_rlbuffer);
+	offset = 0;
 
-  rlCopyADTToBuffer(&theAction, &theBuffer, offset);
+	rlCopyADTToBuffer(&theAction, &clientagent_rlbuffer, offset);
 }
 
 static void onAgentEnd(int theConnection) {
-  reward_t theReward = 0;
+	reward_t theReward = 0;
 
-  /* Read the data in the buffer (data from server) */
-  rlBufferRead(&theBuffer, 0, &theReward, 1, sizeof(reward_t));
+	/* Read the data in the buffer (data from server) */
+	rlBufferRead(&clientagent_rlbuffer, 0, &theReward, 1, sizeof(reward_t));
 
-  /* Call RL method on the recv'd data */
-  agent_end(theReward);
+	/* Call RL method on the recv'd data */
+	agent_end(theReward);
 
-  /* Prepare the buffer for sending data back to the server */
-  rlBufferClear(&theBuffer);
+	/* Prepare the buffer for sending data back to the server */
+	rlBufferClear(&clientagent_rlbuffer);
 }
 
 static void onAgentCleanup(int theConnection) {
-  /* Read the data in the buffer (data from server) */
-  /* No data sent for agent cleanup */
+	/* Read the data in the buffer (data from server) */
+	/* No data sent for agent cleanup */
 
-  /* Call RL method on the recv'd data */
-  agent_cleanup();
+	/* Call RL method on the recv'd data */
+	agent_cleanup();
 
-  /* Prepare the buffer for sending data back to the server */
-  rlBufferClear(&theBuffer);
+	/* Prepare the buffer for sending data back to the server */
+	rlBufferClear(&clientagent_rlbuffer);
 
-  /* Cleanup our resources */
-  free(theObservation.intArray);
-  free(theObservation.doubleArray);
-  free(theTaskSpec);
-  free(theInMessage);
-  
-  theObservation.numInts    = 0;
-  theObservation.numDoubles = 0;
-  theObservation.intArray   = 0;
-  theObservation.doubleArray= 0;
-  theTaskSpec = 0;
-  theInMessage = 0;
-  theInMessageCapacity = 0;
+	/* Cleanup our resources */
+	clearRLStruct(&clientagent_observation);
+	free(theTaskSpec);
+	free(clientagent_inmessage);
+
+	theTaskSpec = 0;
+	clientagent_inmessage = 0;
+	clientagent_inmessagecapacity = 0;
 }
 
 
 static void onAgentMessage(int theConnection) {
-  unsigned int inMessageLength = 0;
-  unsigned int outMessageLength = 0;
-  message_t inMessage = 0;
-  message_t outMessage = 0;
-  unsigned int offset = 0;
+	unsigned int inMessageLength = 0;
+	unsigned int outMessageLength = 0;
+	message_t inMessage = 0;
+	message_t outMessage = 0;
+	unsigned int offset = 0;
 
-  /* Read the data in the buffer (data from server) */
-  offset = 0;
-  offset = rlBufferRead(&theBuffer, offset, &inMessageLength, 1, sizeof(int));
+	/* Read the data in the buffer (data from server) */
+	offset = 0;
+	offset = rlBufferRead(&clientagent_rlbuffer, offset, &inMessageLength, 1, sizeof(int));
 
-  if (inMessageLength >= theInMessageCapacity) {
-    inMessage = (message_t)calloc(inMessageLength+1, sizeof(char));
-    free(theInMessage);
+	if (inMessageLength >= clientagent_inmessagecapacity) {
+		inMessage = (message_t)calloc(inMessageLength+1, sizeof(char));
+		free(clientagent_inmessage);
 
-    theInMessage = inMessage;
-    theInMessageCapacity = inMessageLength;
-  }
+		clientagent_inmessage = inMessage;
+		clientagent_inmessagecapacity = inMessageLength;
+	}
 
-  if (inMessageLength > 0) {
-    offset = rlBufferRead(&theBuffer, offset, theInMessage, inMessageLength, sizeof(char));
-  }
-/*Make sure to null terminate the string */
-   theInMessage[inMessageLength]='\0';
+	if (inMessageLength > 0) {
+		offset = rlBufferRead(&clientagent_rlbuffer, offset, clientagent_inmessage, inMessageLength, sizeof(char));
+	}
+	/*Make sure to null terminate the string */
+	 clientagent_inmessage[inMessageLength]='\0';
 
-  /* Call RL method on the recv'd data */
-  outMessage = agent_message(theInMessage);
-  if (outMessage != NULL) {
-    outMessageLength = strlen(outMessage);
-  }
-  
-  /* Prepare the buffer for sending data back to the server */
-  /* we want to start sending, so we're going to reset the offset to 0 so we write to the beginning of the buffer */
-  rlBufferClear(&theBuffer);
-  offset = 0;
+	/* Call RL method on the recv'd data */
+	outMessage = agent_message(clientagent_inmessage);
+	if (outMessage != NULL) {
+		outMessageLength = strlen(outMessage);
+	}
 
-  offset = rlBufferWrite(&theBuffer, offset, &outMessageLength, 1, sizeof(int)); 
-  if (outMessageLength > 0) {
-    offset = rlBufferWrite(&theBuffer, offset, outMessage, outMessageLength, sizeof(char));
-  }
+	/* Prepare the buffer for sending data back to the server */
+	/* we want to start sending, so we're going to reset the offset to 0 so we write to the beginning of the buffer */
+	rlBufferClear(&clientagent_rlbuffer);
+	offset = 0;
+
+	offset = rlBufferWrite(&clientagent_rlbuffer, offset, &outMessageLength, 1, sizeof(int)); 
+	if (outMessageLength > 0) {
+		offset = rlBufferWrite(&clientagent_rlbuffer, offset, outMessage, outMessageLength, sizeof(char));
+	}
 }
 
 static void runAgentEventLoop(int theConnection) {
   int agentState = 0;
 
   do {
-    rlBufferClear(&theBuffer);
-    rlRecvBufferData(theConnection, &theBuffer, &agentState);
+    rlBufferClear(&clientagent_rlbuffer);
+    rlRecvBufferData(theConnection, &clientagent_rlbuffer, &agentState);
 
     switch(agentState) {
     case kAgentInit:
@@ -227,61 +227,61 @@ static void runAgentEventLoop(int theConnection) {
       break;
     };
 
-    rlSendBufferData(theConnection, &theBuffer, agentState);
+    rlSendBufferData(theConnection, &clientagent_rlbuffer, agentState);
   } while (agentState != kRLTerm);
 }
 
 int main(int argc, char** argv) {
-  int theConnection = 0;
+	int theConnection = 0;
 
-  const char *usage = "The following environment variables are used by the agent to control its function:\n"
-    "RLGLUE_HOST  : If set the agent will use this ip or hostname to connect to rather than %s\n"
-	"RLGLUE_PORT  : If set the agent will use this port to connect on rather than %d\n";
-  
-  struct hostent *host_ent;
+	const char *usage = "The following environment variables are used by the agent to control its function:\n"
+	  	"RLGLUE_HOST  : If set the agent will use this ip or hostname to connect to rather than %s\n"
+		"RLGLUE_PORT  : If set the agent will use this port to connect on rather than %d\n";
 
-  char* host = kLocalHost;
-  short port = kDefaultPort;
+	struct hostent *host_ent;
 
-  char* envptr = 0;
+	char* host = kLocalHost;
+	short port = kDefaultPort;
 
-  if (argc > 1) {
-    fprintf(stderr, usage, kLocalHost, kDefaultPort);
-    exit(1);
-  }
+	char* envptr = 0;
 
-  host = getenv("RLGLUE_HOST");
-  if (host == 0) {
-    host = kLocalHost;
-  }
+	if (argc > 1) {
+		fprintf(stderr, usage, kLocalHost, kDefaultPort);
+		exit(1);
+	}
 
-  envptr = getenv("RLGLUE_PORT");  
-  if (envptr != 0) {
-    port = strtol(envptr, 0, 10);
-    if (port == 0) {
-      port = kDefaultPort;
-    }
-  }
+	host = getenv("RLGLUE_HOST");
+	if (host == 0) {
+		host = kLocalHost;
+	}
 
-  if (isalpha(host[0])) {
-    host_ent = gethostbyname(host); 
-    host = inet_ntoa(*(struct in_addr*)host_ent->h_addr);
-  }
+	envptr = getenv("RLGLUE_PORT");  
+	if (envptr != 0) {
+		port = strtol(envptr, 0, 10);
+		if (port == 0) {
+			port = kDefaultPort;
+		}
+	}
 
-  fprintf(stdout, "RL-Glue C Agent Codec Version %s, Build %s\n\tConnecting to host=%s on port=%d...\n", VERSION,__rlglue_get_svn_version(),host, port);
-  fflush(stdout);
-  
-  /* Allocate what should be plenty of space for the buffer - it will dynamically resize if it is too small */
-  rlBufferCreate(&theBuffer, 4096);
-  
-    theConnection = rlWaitForConnection(host, port, kRetryTimeout);
+	if (isalpha(host[0])) {
+		host_ent = gethostbyname(host); 
+		host = inet_ntoa(*(struct in_addr*)host_ent->h_addr);
+	}
+
+	fprintf(stdout, "RL-Glue C Agent Codec Version %s, Build %s\n\tConnecting to host=%s on port=%d...\n", VERSION,__rlglue_get_svn_version(),host, port);
+	fflush(stdout);
+
+	/* Allocate what should be plenty of space for the buffer - it will dynamically resize if it is too small */
+	rlBufferCreate(&clientagent_rlbuffer, 4096);
+
+	theConnection = rlWaitForConnection(host, port, kRetryTimeout);
 	fprintf(stdout, "\tRL-Glue C Agent Codec :: Connected\n");
-    rlBufferClear(&theBuffer);
-    rlSendBufferData(theConnection, &theBuffer, kAgentConnection);
-    runAgentEventLoop(theConnection);
-    rlClose(theConnection);
+	rlBufferClear(&clientagent_rlbuffer);
+	rlSendBufferData(theConnection, &clientagent_rlbuffer, kAgentConnection);
+	runAgentEventLoop(theConnection);
+	rlClose(theConnection);
 
-  rlBufferDestroy(&theBuffer);
+	rlBufferDestroy(&clientagent_rlbuffer);
 
-  return 0;
+	return 0;
 }
