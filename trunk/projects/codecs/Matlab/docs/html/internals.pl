53ff16df0f389918b90d9eec4e8ad16b137a256c
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2009-01-31 19:53:52 +0000

    Updated the Matlab docs to have all the info about the sample mines project, the installer, the task spec parser, etc.

--- trunk/projects/codecs/Matlab/docs/html/internals.pl
+++ trunk/projects/codecs/Matlab/docs/html/internals.pl
@@ -2,7 +2,23 @@
 # Associate internals original text with physical files.
 
 
+$key = q/mines-sample/;
+$ref_files{$key} = "$dir".q|MatlabCodec.html|; 
+$noresave{$key} = "$nosave";
+
 $key = q/sec:env/;
+$ref_files{$key} = "$dir".q|MatlabCodec.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sarsa-sample/;
+$ref_files{$key} = "$dir".q|MatlabCodec.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/mines-sarsa-sample/;
+$ref_files{$key} = "$dir".q|MatlabCodec.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/one-at-a-time/;
 $ref_files{$key} = "$dir".q|MatlabCodec.html|; 
 $noresave{$key} = "$nosave";
 
