c64aab546528cecba64f5d9e97654ab2d910e87c
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2009-02-02 17:30:00 +0000

    Created the Matlab-Codec builder.

--- trunk/projects/distribution_tools/Glue-and-C-Codec/prepare-mac-package.bash
+++ trunk/projects/distribution_tools/Glue-and-C-Codec/prepare-mac-package.bash
@@ -13,5 +13,6 @@ rm $MACDIR/*.dmg
 /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker --root install_root --id org.rlcommunity.rlglue --version 3.0 --title "RL-Glue 3.0 and C/C++ Codec" --resources $MACDIR/resources --out $MACDIR/$PACKAGENAME --root-volume-only
 
 #Create the disk image
+rm $DISTDIR/$IMAGENAME
 hdiutil create -srcfolder $MACDIR/$PACKAGENAME $DISTDIR/$IMAGENAME
 rm -Rf $MACDIR/*.pkg
