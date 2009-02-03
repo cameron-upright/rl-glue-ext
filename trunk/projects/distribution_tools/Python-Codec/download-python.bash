b2ecb1c66c072cb6e89b4fde1d53fd5611a1b89f
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2009-02-03 05:24:51 +0000

    The Python codec now updates the wiki and uploads.

--- trunk/projects/distribution_tools/Python-Codec/download-python.bash
+++ trunk/projects/distribution_tools/Python-Codec/download-python.bash
@@ -25,9 +25,22 @@ tar -cf $DEVTAR $DEVDIR
 tar -cf $USERTAR $USERDIR
 gzip $DEVTAR
 gzip $USERTAR
- 
+
 mv *.gz $DISTDIR
+DEVZIP=$DISTDIR/$DEVTAR.gz
+USERZIP=$DISTDIR/$USERTAR.gz
 
 rm -Rf $USERDIR
 rm -Rf $DEVDIR
 
+#Upload to Google Code
+python ../googlecode_upload.py -s "Developer version of the RL-Glue Python Codec $VERSION" -p rl-glue-ext --labels=Type-Installer,OpSys-All,Language-Python,Audience-Dev $DEVZIP
+python ../googlecode_upload.py -s "End-User version of the RL-Glue Python Codec $VERSION" -p rl-glue-ext --labels=Type-Installer,OpSys-All,Language-Python,Audience-User $USERZIP
+
+# Update the Wiki
+python substitute-python-strings.py $VERSION $DEVDIR-$VERSION $USERDIR-$VERSION
+cp Python.wiki ../wiki/python.new
+cd ../wiki
+svn up
+mv python.new Python.wiki
+svn commit Python.wiki -m "Automated update of Python wiki page."
