a1e30c956ee43580a76e92e28c0c1b81b0ebf481
brian <brian@tannerpages.com>
2009-02-03 07:22:37 +0000

    Small updates to all of the Wiki templates.

--- projects/distribution_tools/create-all.bash
+++ projects/distribution_tools/create-all.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 MATLAB_VERSION=1.0-RC-FINAL-1
 C_CODEC_VERSION=2.0-RC-FINAL-1
+LISP_VERSION=1.0-RC-FINAL-1
 cd Glue-and-C-Codec
 bash create-all-dist.bash $C_CODEC_VERSION
 cd ..
@@ -17,3 +18,6 @@ cd Python-Codec
 bash download-python.bash
 cd ..
 
+cd Lisp-Codec
+bash download-lisp.bash $LISP_VERSION
+cd ..
