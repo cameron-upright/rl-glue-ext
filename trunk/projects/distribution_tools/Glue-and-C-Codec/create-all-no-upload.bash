98c3f93ab663200547d6ca9dc296381ee6c3012d
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2009-02-03 16:26:05 +0000

    Created a dist script for Glue and C codec to build the dist stuff but not upload it.

---
+++ trunk/projects/distribution_tools/Glue-and-C-Codec/create-all-no-upload.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -z $1 ]
+then
+  CODECVERSION=UNKNOWN
+  echo "You didn't pass a version for the codec so using UNKNOWN"
+else
+  CODECVERSION=$1
+fi
+
+bash ./download-build-glue.bash
+bash ./download-build-codec.bash
+bash ./create-glue-dist.bash
+bash ./create-codec-dist.bash
+bash ./prepare-mac-package.bash
+bash ./cleanup.bash
