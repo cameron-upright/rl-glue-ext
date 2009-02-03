d06e307bada1a4e6bea037b8f4e37624cf2ae9c0
brian <brian@tannerpages.com>
2009-02-03 05:04:21 +0000

    Finally got the Matlab uploading and wiki how I like it.

--- projects/distribution_tools/Matlab-Codec/substitute-matlab-strings.py
+++ projects/distribution_tools/Matlab-Codec/substitute-matlab-strings.py
@@ -2,7 +2,8 @@ import sys
 
 
 theVersion=sys.argv[1]
-uploadedFileName=sys.argv[2]
+uploadedFileBase=sys.argv[2]
+uploadedFileName=uploadedFileBase+".tar.gz"
 theLink="http://rl-glue-ext.googlecode.com/files/"+uploadedFileName
 detailsLink="http://code.google.com/p/rl-glue-ext/downloads/detail?name="+uploadedFileName
 
@@ -13,6 +14,7 @@ subs={}
 subs['MATLAB-USER-DEV-VERSION']=theVersion
 subs['MATLAB-USER-DEV-LINK']=theLink
 subs['MATLAB-USER-DEV-DETAILS-LINK']=detailsLink
+subs['MATLAB-USER-DEV-FILE-BASE']=uploadedFileBase
 f = file(fileName)
 newlines = []
 for line in f:
