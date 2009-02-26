9a9c01cc0a6627de9cae960ca6a213f93efbf21f
brian@tannerpages.com <brian@tannerpages.com@902a3cd8-c54e-0410-8468-0d8400517545>
2009-02-26 07:24:38 +0000

    latest greatest version of all of the scripts and updated control stating that we don't do the codec.

--- trunk/projects/distribution_tools/Glue-and-C-Codec/update-wiki-gluecore-partial-rpm64.bash
+++ trunk/projects/distribution_tools/Glue-and-C-Codec/update-wiki-gluecore-partial-rpm64.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Update the RPM Wiki Template
 VERSION=`./install_root/usr/local/bin/rl_glue --pv`
-FILENAME=rl-glue-${VERSION}-2_amd64.rpm
+FILENAME=rl-glue-${VERSION}-2.amd64.rpm
 python filesubstitution.py $VERSION $FILENAME wiki/templates/RLGlueCore.wiki.rpm64.template wiki/current/RLGlueCore.wiki.rpm64
 
 
