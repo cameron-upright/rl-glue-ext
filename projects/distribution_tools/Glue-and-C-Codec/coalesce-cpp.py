47257a589cc02229f32362c63dc5e9476c30b631
brian <brian@tannerpages.com>
2009-03-07 18:23:16 +0000

    Updated the codoec coalesce file to use the new templates.

--- projects/distribution_tools/Glue-and-C-Codec/coalesce-cpp.py
+++ projects/distribution_tools/Glue-and-C-Codec/coalesce-cpp.py
@@ -1,7 +1,7 @@
 import sys
 
 theDir="wiki/current";
-theFiles=['CandCPP.wiki.header','CandCPP.wiki.fromsource','RLGlueCore.wiki.macbinary','CandCPP.wiki.footer'];
+theFiles=['CandCPP.wiki.header','CandCPP.wiki.fromsource','RLGlueCore.wiki.macbinary','CandCPP.wiki.linux','CandCPP.wiki.debian32','CandCPP.wiki.debian64','CandCPP.wiki.rpm32','CandCPP.wiki.rpm64','CandCPP.wiki.footer'];
 
 outfileName="wiki/current/CandCPP.wiki"
 outfile = file(outfileName, 'w')
