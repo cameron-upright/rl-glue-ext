d9d513e2cc9e181b8dca43b0b525b5d4691d3848
brian <brian@tannerpages.com>
2009-03-10 04:06:23 +0000

    Final fix to deb32 script.

--- projects/distribution_tools/Glue-and-C-Codec/do-standalone-debian-32.bash
+++ projects/distribution_tools/Glue-and-C-Codec/do-standalone-debian-32.bash
@@ -6,7 +6,7 @@ bash ./download-build-codec.bash
 bash ./create-glue-dist.bash
 bash ./create-codec-dist.bash
 #This could be 32 or 64 
-sh ./create-debian-package.bash 
+sh ./create-debian-package.sh 
 bash ./update-wiki-gluecore-partial-deb32.bash
 bash ./update-wiki-gluecore-partial-rpm32.bash
 bash ./create-c-codec-deb.bash
