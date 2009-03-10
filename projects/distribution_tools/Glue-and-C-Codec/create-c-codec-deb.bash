122282aa5ec0102f354428e5b72100b98f40e979
brian <brian@tannerpages.com>
2009-03-10 02:53:34 +0000

    New scripts to build debs for codec and glue.

--- projects/distribution_tools/Glue-and-C-Codec/create-c-codec-deb.bash
+++ projects/distribution_tools/Glue-and-C-Codec/create-c-codec-deb.bash
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-# deb=>rpm FROM SCRATCH (tar.gz required only) Script for RL-Glue
-# Version: v1.1 - modified 10-3-2009
+# deb=>rpm FROM SCRATCH (tar.gz required only) Script for RL-Glue C/C++ Codec
+# Version: v1.0 - modified 10-3-2009
 # Author: Iordanis Daroglou <idaroglo@csd.auth.gr>
 # Supported build arches: i386, amd64
 # Virtually any arch on which Debian is available could be supported..
@@ -16,23 +16,28 @@ TSTAMPNUM=$(date +"%Y%m%d_%H%M")
 # the tar.gz sources will be copied and extracted here
 # these dirs have to be created in the svn-repo, the old ones can be removed
 DEBBASE_DIR=$BASEDIR/debian/buildbase
-DEB_CFG=$BASEDIR/debian/debcfg/glue
+DEB_CFG=$BASEDIR/debian/debcfg/codec
 
 # target dir, where the debs & rpms end up - also the place where we get the source tar.gz from
 DIST=$BASEDIR/dist
-rm $DIST/*.rpm
-rm $DIST/*.deb
 
-# variables for the filenames of the lintian logs - (un)comment according to arch
+# get latest upstream distribution
+#==================================
+bash download-build-codec.bash
+bash create-codec-dist.bash
+
+# variables for the filenames of the lintian check logs - (un)comment according to arch
 # if we want no logs, we can ommit them
+#  -- lintian related stuff temporarily commented out (see below why)..
 THE_ARCH=$(uname -m)
 if [ $THE_ARCH = x86_64 ]; then
-	echo "Building 64 Bit Packages..."
-#	LINTLOG=lintian-glue-64_$TSTAMPNUM.log
+        echo "Building 64 Bit Packages..."
+#        LINTLOG=lintian-ccodec-64_$TSTAMPNUM.log
 else
-	echo "Building 32 Bit Packages..."
-#	LINTLOG=lintian-glue-32_$TSTAMPNUM.log
+        echo "Building 32 Bit Packages..."
+#        LINTLOG=lintian-ccodec-32_$TSTAMPNUM.log
 fi
+
 
 # prepare the updated package debian sources (section 'identical' for both arches)
 #==================================================================================
@@ -40,12 +45,12 @@ cp $DIST/*.tar.gz $DEBBASE_DIR
 cd $DEBBASE_DIR
 tar -xzf *.tar.gz
 
-# this one renames the rlglue-XX folder to rl-glue-XX, no need to know about version numbers etc.
+# this one renames the folder in the appropriate way (rl-glue-c-codec-XX), no need to know about version numbers etc.
 # since we cannot do find -name bla -exec cd {} \;, we have to do some tricks to get the versioned dir
-DIR_NAME=$(find -maxdepth 1 -type d -name "rlg*")
+DIR_NAME=$(find -maxdepth 1 -type d -name "c-codec*")
 echo $DIR_NAME > tmpdir
-sed -i "s/.\/rl/rl-/" tmpdir
-sed -e "s/rl-glue-//" tmpdir > tmpver
+sed -i "s/.\/c-codec/rl-glue-c-codec/" tmpdir
+sed -e "s/rl-glue-c-codec-//" tmpdir > tmpver
 DASHED=$(cat tmpdir)
 VER=$(cat tmpver)
 
@@ -53,7 +58,7 @@ DASHED_DIR=$DEBBASE_DIR/$DASHED
 mv $DIR_NAME $DASHED
 rm tmp*
 
-# cd to the rl-glue-XX dir and build the package
+# cd to the rl-glue-c-codec-XX dir and build the package
 cd $DASHED_DIR
 mkdir debian
 cd $DASHED_DIR/debian
@@ -62,8 +67,8 @@ cp $DEB_CFG/* .
 # before we start building, we change the timestamps and version no. in copyright/changelog
 #===========================================================================================
 sed -i "s/X/$VER/" changelog
-sed -i "s/Wed, 11 Feb 2009 03:20:37 +0200/$TSTAMP/" changelog
-sed -i "s/Wed Feb 25 18:43:29 EET 2009/$TSTAMP/" copyright
+sed -i "s/autodate/$TSTAMP/" changelog
+sed -i "s/autodate/$TSTAMP/" copyright
 
 #  actual building takes place here
 #====================================
@@ -76,13 +81,14 @@ dpkg-buildpackage -rfakeroot -uc -us
 # WARNING: deb check temporarily disabled due to some lintian issues (v2.2.7)
 #============================================================================
 #cd ..
-#lintian rl-glue*.deb > $DIST/$LINTLOG
+#lintian rl-glue-c*.deb > $DIST/$LINTLOG
 cd $DASHED_DIR
-fakeroot alien -r -k ../rl-glue*.deb
+fakeroot alien -r -k ../rl-glue-c-codec*.deb
 cd ..
-cp rl-glue*.deb $DIST
-mv $DASHED_DIR/rl-glue*.rpm $DIST
+cp rl-glue-c*.deb $DIST
+mv $DASHED_DIR/rl-glue-c-codec*.rpm $DIST
 
 #cleanup to prepare for next build :)
 cd $DEBBASE_DIR
 rm -r *
+
