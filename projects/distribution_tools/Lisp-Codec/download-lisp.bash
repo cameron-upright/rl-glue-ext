6151b59e8ea5c7b3d60fd424fafe9a5807e219a1
gabalz <gabalz@902a3cd8-c54e-0410-8468-0d8400517545>
2009-03-14 21:10:50 +0000

    Redistribution of Lisp Codec 1.0.

--- projects/distribution_tools/Lisp-Codec/download-lisp.bash
+++ projects/distribution_tools/Lisp-Codec/download-lisp.bash
@@ -1,42 +1,104 @@
 #!/bin/bash
-#
-# Download Lisp
-#
+
 if [ -z $1 ]
 then
   VERSION=UNKNOWN
-  echo "You didn't pass a version so using UNKNOWN"
+  echo "You must pass a version!"
+  exit 1
 else
   VERSION=$1
 fi
 
-DEVDIR=lisp-codec
+DIRBASE=lisp-codec
+FULLDIR=${DIRBASE}-full
+ASDFDIR=${DIRBASE}-asdf
+DEVDIR=${DIRBASE}-dev
 DISTDIR=dist
 
-rm -Rf $DEVDIR
-rm -Rf $DISTDIR
-svn export http://rl-glue-ext.googlecode.com/svn/trunk/projects/codecs/Lisp $DEVDIR
+rm -Rf ${FULLDIR}
+rm -Rf ${ASDFDIR}
+rm -Rf ${DEVDIR}
+rm -Rf ${DISTDIR}
 
-mkdir $DISTDIR
-cp $DEVDIR/doc/manual/lisp-codec.pdf $DEVDIR/
+# Making the development snapshot.
 
-DEVTAR=$DEVDIR-$VERSION.tar
+svn export http://rl-glue-ext.googlecode.com/svn/trunk/projects/codecs/Lisp ${DEVDIR}
 
-tar -cf $DEVTAR $DEVDIR
-gzip $DEVTAR
-mv *.gz $DISTDIR/
-ZIPNAME=$DEVTAR.gz
-DEVZIP=$DISTDIR/$ZIPNAME
+# Preparing the *asdf* distribution.
 
-rm -Rf $DEVDIR
+mkdir ${ASDFDIR}
+cp -R ${DEVDIR}/src/* ${ASDFDIR}
+cp -R ${DEVDIR}/examples ${ASDFDIR}
+mv ${ASDFDIR}/examples/ ${ASDFDIR}/rl-glue-examples/
+mkdir ${ASDFDIR}/doc
+cp ${DEVDIR}/doc/manual/lisp-codec.pdf ${ASDFDIR}/doc
+cp -R ${DEVDIR}/doc/reference ${ASDFDIR}/doc
 
-python ../googlecode_upload.py -s "RL-Glue Lisp Codec $VERSION" -p rl-glue-ext --labels=Type-Installer,OpSys-All,Language-Lisp,Audience-User,Audience-Dev $DEVZIP
+# Preparing the *full* distribution.
 
+mkdir ${FULLDIR}
+mkdir ${FULLDIR}/asdf
+cp -R ${ASDFDIR}/* ${FULLDIR}/asdf
+mv ${FULLDIR}/asdf/doc/ ${FULLDIR}/
+cp setup.lisp ${FULLDIR}
 
-# Update the Wiki
-python substitute-lisp-strings.py $VERSION $DEVDIR-$VERSION
+cvs -f -d:pserver:anonymous:@cclan.cvs.sourceforge.net:/cvsroot/cclan login
+cvs -fz3 -d:pserver:anonymous@cclan.cvs.sourceforge.net:/cvsroot/cclan export -D "1 day ago" asdf
+cvs -f -d:pserver:anonymous@cclan.cvs.sourceforge.net:/cvsroot/cclan logout
+
+mkdir ${FULLDIR}/asdf
+mv asdf/asdf.lisp ${FULLDIR}/asdf/
+rm -Rf asdf
+
+SPLIT_SEQUENCE=split-sequence
+USOCKET=usocket-0.4.1
+
+wget -P ${FULLDIR}/asdf http://ftp.linux.org.uk/pub/lisp/experimental/cclan/${SPLIT_SEQUENCE}.tar.gz
+wget -P ${FULLDIR}/asdf http://common-lisp.net/project/usocket/releases/${USOCKET}.tar.gz
+
+for f in ${SPLIT_SEQUENCE} ${USOCKET}; do
+    gzip -d ${FULLDIR}/asdf/${f}.tar.gz
+    tar -xvf ${FULLDIR}/asdf/${f}.tar -C ${FULLDIR}/asdf
+    rm -f ${FULLDIR}/asdf/${f}.tar
+done
+
+# Creating the distributions.
+
+mkdir ${DISTDIR}
+
+DEVFILEBASE=${DEVDIR}-${VERSION}
+DEVTAR=${DEVFILEBASE}.tar
+tar -cf ${DEVTAR} ${DEVDIR}
+gzip ${DEVTAR}
+
+FULLFILEBASE=${FULLDIR}-${VERSION}
+FULLTAR=${FULLFILEBASE}.tar
+tar -cf ${FULLTAR} ${FULLDIR}
+gzip ${FULLTAR}
+
+ASDFFILEBASE=${ASDFDIR}-${VERSION}
+ASDFTAR=${ASDFFILEBASE}.tar
+tar -cf ${ASDFTAR} ${ASDFDIR}
+gzip ${ASDFTAR}
+
+mv *.tar.gz ${DISTDIR}/
+
+rm -Rf ${DEVDIR}
+rm -Rf ${FULLDIR}
+rm -Rf ${ASDFDIR}
+
+# Uploading the distributions.
+
+python ../googlecode_upload.py -s "RL-Glue Lisp Codec Full $VERSION" -p rl-glue-ext --labels=Type-Installer,OpSys-All,Language-Lisp,Audience-User ${DISTDIR}/${FULLTAR}.gz
+python ../googlecode_upload.py -s "RL-Glue Lisp Codec ASDF $VERSION" -p rl-glue-ext --labels=Type-Installer,OpSys-All,Language-Lisp,Audience-User ${DISTDIR}/${ASDFTAR}.gz
+python ../googlecode_upload.py -s "RL-Glue Lisp Codec Dev $VERSION" -p rl-glue-ext --labels=Type-Installer,OpSys-All,Language-Lisp,Audience-Dev ${DISTDIR}/${DEVTAR}.gz
+
+# Updating the Wiki.
+
+python substitute-lisp-strings.py $VERSION ${FULLFILEBASE} ${ASDFFILEBASE} ${DEVFILEBASE} 
 cp Lisp.wiki ../wiki/lisp.new
 cd ../wiki
 svn up
 mv lisp.new Lisp.wiki
 svn commit Lisp.wiki -m "Automated update of Lisp wiki page."
+
--- projects/distribution_tools/Lisp-Codec/substitute-lisp-strings.py
+++ projects/distribution_tools/Lisp-Codec/substitute-lisp-strings.py
@@ -2,19 +2,35 @@ import sys
 
 
 theVersion=sys.argv[1]
-uploadedFileBase=sys.argv[2]
-uploadedFileName=uploadedFileBase+".tar.gz"
-theLink="http://rl-glue-ext.googlecode.com/files/"+uploadedFileName
-detailsLink="http://code.google.com/p/rl-glue-ext/downloads/detail?name="+uploadedFileName
+uploadedFullFileBase=sys.argv[2]
+uploadedAsdfFileBase=sys.argv[3]
+uploadedDevFileBase=sys.argv[4]
+
+uploadedFullFileName=uploadedFullFileBase+".tar.gz"
+uploadedAsdfFileName=uploadedAsdfFileBase+".tar.gz"
+uploadedDevFileName=uploadedDevFileBase+".tar.gz"
+
+fullLink="http://rl-glue-ext.googlecode.com/files/"+uploadedFullFileName
+fullDetailsLink="http://code.google.com/p/rl-glue-ext/downloads/detail?name="+uploadedFullFileName
+asdfLink="http://rl-glue-ext.googlecode.com/files/"+uploadedAsdfFileName
+asdfDetailsLink="http://code.google.com/p/rl-glue-ext/downloads/detail?name="+uploadedAsdfFileName
+devLink="http://rl-glue-ext.googlecode.com/files/"+uploadedDevFileName
+devDetailsLink="http://code.google.com/p/rl-glue-ext/downloads/detail?name="+uploadedDevFileName
 
 fileName="Lisp.wiki.template"
 outfileName="Lisp.wiki"
 
 subs={}
-subs['LISP-USER-DEV-VERSION']=theVersion
-subs['LISP-USER-DEV-LINK']=theLink
-subs['LISP-USER-DEV-DETAILS-LINK']=detailsLink
-subs['LISP-USER-DEV-FILE-BASE']=uploadedFileBase
+subs['LISP-CODEC-VERSION']=theVersion
+subs['LISP-FULL-LINK']=fullLink
+subs['LISP-FULL-DETAILS-LINK']=fullDetailsLink
+subs['LISP-FULL-FILE-BASE']=uploadedFullFileBase
+subs['LISP-ASDF-LINK']=asdfLink
+subs['LISP-ASDF-DETAILS-LINK']=asdfDetailsLink
+subs['LISP-ASDF-FILE-BASE']=uploadedAsdfFileBase
+subs['LISP-DEV-LINK']=devLink
+subs['LISP-DEV-DETAILS-LINK']=devDetailsLink
+subs['LISP-DEV-FILE-BASE']=uploadedDevFileBase
 f = file(fileName)
 newlines = []
 for line in f:
